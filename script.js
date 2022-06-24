db4021fd23073448f5e600fc2944ed61cd3c22ff
Tianyi Wang <60373981+betterForFun@users.noreply.github.com>
2022-06-24 16:23:01 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,50 +1,25 @@
 'use strict';
 
-function checkClicked() {
-  myGuess = Number(input.value);
-  if (!myGuess) {
-    displayMessage.textContent = 'Put in a guess Number';
-  } else if (startScore == 0) {
-    displayMessage.textContent = 'You lost the game :(';
-  } else if (myGuess > target) {
-    displayMessage.textContent = 'Too high!';
-    startScore--;
-    score.textContent = startScore;
-  } else if (myGuess < target) {
-    displayMessage.textContent = 'Too low!';
-    startScore--;
-    score.textContent = startScore;
-  } else {
-    displayMessage.textContent = 'Congradulations! 💐';
-    targetDisplay.textContent = target;
-    if (startScore >= highScore.textContent) {
-      highScore.textContent = startScore;
-    }
-    document.querySelector('body').style.backgroundColor = 'green';
-    document.querySelector('.number').style.width = '30rem';
+let click = document.querySelectorAll('.show-modal');
+let shotModel = document.querySelector('.modal');
+let overLay = document.querySelector('.overlay');
+let close = document.querySelector('.close-modal');
+
+for (let x = 0; x < click.length; x++) {
+  click[x].addEventListener('click', function () {
+    shotModel.classList.remove('hidden');
+    overLay.classList.remove('hidden');
+  });
+}
+
+close.addEventListener('click', function () {
+  shotModel.classList.add('hidden');
+  overLay.classList.add('hidden');
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key == 'Escape') {
+    shotModel.classList.add('hidden');
+    overLay.classList.add('hidden');
   }
-}
-
-function reset() {
-  input.value = ' ';
-  startScore = 20;
-  score.textContent = startScore;
-  target = Math.trunc(Math.random() * 20) + 1;
-  targetDisplay.textContent = '?';
-  displayMessage.textContent = 'Start Guessing ..';
-  document.querySelector('body').style.backgroundColor = 'black';
-  document.querySelector('.number').style.width = '15rem';
-}
-
-let startScore = 20;
-let myGuess;
-let target = Math.trunc(Math.random() * 20) + 1;
-let score = document.querySelector('.score');
-let highScore = document.querySelector('.highscore');
-let displayMessage = document.querySelector('.message');
-let targetDisplay = document.querySelector('.number');
-let input = document.querySelector('.guess');
-let button = document.querySelector('.check');
-let button2 = document.querySelector('.again');
-button.addEventListener('click', checkClicked);
-button2.addEventListener('click', reset);
+});
